8f0b63845ffdef46643fa1783c6493299a8ea18c
Eugeniusz <104994034+eugeniusz57@users.noreply.github.com>
2023-10-24 19:04:36 +0200

    create new user

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,18 +1,46 @@
-import { NextResponse } from "next/server";
+import {  NextResponse } from "next/server";
 import db from "@/db";
+import { hash } from "bcrypt";
 
 // create User
-export async function POST(req: Request) {
-  const body = await req.json();
-  const { login, tel, password, email } = body;
 
-  const response = await db.query(
-    "INSERT INTO users (login, tel, password, email) VALUES ($1, $2, $3, $4) RETURNING *",
-    [login, tel, password, email]
-  );
-  const user = response[0];
-  return NextResponse.json({ user: user }, { status: 201 });
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { user_login, tel, user_password, email } = body;
+    const hashedPassword =  await hash( user_password, 10);
+  
+    const newUser = await db.query(
+      "INSERT INTO users (user_login, tel, user_password, email) VALUES ($1, $2, $3, $4) RETURNING *",
+      [user_login, tel, hashedPassword, email]
+    );
+  
+   
+    
+    const user = newUser.rows[0];
+    const {user_password:NewUserPassword, ...rest} = user;
+  
+    
+    return NextResponse.json({user:rest, message: 'User Created successfully'  }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ message: 'Something went wrong!'}, {status: 50});
+  }
+ 
 }
 
 // get all users
-export async function GET(req: Request) {}
+export async function GET() {
+  
+  try {
+    const allUser = await db.query(
+      "SELECT * FROM users"
+    )
+  
+    const data = allUser.rows;
+    return NextResponse.json({data},{status: 200 });
+  } catch (error) {
+    return NextResponse.json({ message: 'Something went wrong!'}, {status: 50});
+  }
+}
+
