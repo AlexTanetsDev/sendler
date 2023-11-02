0648a60f0147dc8a1b14ccc84d87a2ea574e2deb
Eugeniusz <104994034+eugeniusz57@users.noreply.github.com>
2023-11-02 19:58:39 +0100

    add coloms user_name and user_active

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -7,7 +7,7 @@ import { hash } from "bcrypt";
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-    const { user_login, tel, user_password, email } = body;
+    const { user_login, tel, user_password, email, user_name } = body;
     const hashedPassword = await hash(user_password, 10);
 
     //existing user by email
@@ -23,34 +23,49 @@ export async function POST(req: Request) {
       );
     }
 
-    //existing user by name
-    const userName = await db.query(
+    //existing user by login
+    const userLogin = await db.query(
       "SELECT * FROM users WHERE user_login = $1",
       [user_login]
     );
-    const existingUserByName = userName.rows[0];
+    const existingUserByLogin = userLogin.rows[0];
 
-    if (existingUserByName) {
+    if (existingUserByLogin) {
       return NextResponse.json(
-        { users: null, message: "User with this user name already exists" },
+        { users: null, message: "User with this login already exists" },
         { status: 409 }
       );
     }
 
     //existing user by phone
     const userTel = await db.query("SELECT * FROM users WHERE tel = $1", [tel]);
-    const existingUserByTel = userTel.rows[0];
+    const existingUserByName = userTel.rows[0];
 
-    if (existingUserByTel) {
+    if (existingUserByName) {
       return NextResponse.json(
         { users: null, message: "User with this phone already exists" },
         { status: 409 }
       );
     }
+
+    //existing user by user name
+    const userName = await db.query(
+      "SELECT * FROM users WHERE user_name = $1",
+      [user_name]
+    );
+    const existingUserByTel = userName.rows[0];
+
+    if (existingUserByTel) {
+      return NextResponse.json(
+        { users: null, message: "User with this name already exists" },
+        { status: 409 }
+      );
+    }
+
     //create new user
     const newUser = await db.query(
-      "INSERT INTO users (user_login, tel, user_password, email) VALUES ($1, $2, $3, $4) RETURNING *",
-      [user_login, tel, hashedPassword, email]
+      "INSERT INTO users (user_login, tel, user_password, email, user_name) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [user_login, tel, hashedPassword, email, user_name]
     );
 
     const userCreate = newUser.rows[0];
