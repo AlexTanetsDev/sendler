4228f3d6a329b8d27899e79b27fd87eaa1cdb9b0
Eugeniusz <104994034+eugeniusz57@users.noreply.github.com>
2023-12-08 19:24:10 +0100

    change navbar and add style for login and signup form

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,6 +1,5 @@
 "use client";
 import { useSession } from "next-auth/react";
-import { signOut } from "next-auth/react";
 import Link from "next/link";
 import LogOutButton from "./LogOutButton";
 
@@ -12,50 +11,50 @@ const navigation = [
   { id: 5, title: "Dashboard", path: "/dashboard" },
 ];
 
+const publicNavigation = [
+  { id: 1, title: "Головна", path: "/" },
+  { id: 2, title: "Про нас", path: "/about" },
+  { id: 3, title: "Ціни", path: "/prices" },
+  { id: 4, title: "Питання", path: "/questions" },
+  { id: 5, title: "Послуги", path: "/services" },
+  { id: 6, title: "Контакти", path: "/contacts" },
+];
+
+const privateNavigation = [
+  { id: 1, title: "Головна", path: "/" },
+  { id: 2, title: "Розсилка", path: "/mailing-list" },
+  { id: 3, title: "Управління контактами", path: "/assignment-of-contacts" },
+  { id: 4, title: "Статистика", path: "/statistics" },
+  { id: 5, title: "Особистий кабінет", path: "/profil" },
+];
+
 const Nav = () => {
   const { data: session, status } = useSession();
-  console.log("sessionUserNav", session);
-  console.log("statusNav", status);
   return (
     <>
-      {" "}
-      <nav className="bg-white border-gray-200 dark:bg-gray-900">
-        <div className="flex flex-wrap items-center justify-between max-w-screen-xl p-4 mx-auto">
-          <Link href="/" className="flex items-center">
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              NextAuthApp
-            </span>
-          </Link>
-          {status === "authenticated" ? (
-              <LogOutButton/>
-          ) : (
-            <>
-              <div className="flex items-center">
-                <Link
-                  href="/signup"
-                  className="mr-6 text-sm text-gray-500 dark:text-white hover:underline"
-                >
-                  Register
-                </Link>
-                <Link
-                  href="/api/auth/signin"
-                  className="text-sm text-blue-600 dark:text-blue-500 hover:underline"
-                >
-                  Login
-                </Link>
-              </div>
-            </>
-          )}
-        </div>
-      </nav>
       <nav className="grid grid-cols-3">
         <div>Sendler Logo</div>
-        <div className="grid grid-cols-5 gap-6">
-          {navigation.map(({ id, title, path }) => (
-            <Link key={id} href={path}>
-              {title}
-            </Link>
-          ))}
+        <div
+          className={`grid  gap-6 ${
+            status === "authenticated" ? "grid-cols-6" : "grid-cols-7"
+          }`}
+        >
+          {status === "authenticated"
+            ? privateNavigation.map(({ id, title, path }) => (
+                <Link key={id} href={path} className="hover:underline hover:underline-offset-4 py-6">
+                  {title}
+                </Link>
+              ))
+            : publicNavigation.map(({ id, title, path }) => (
+                <Link key={id} href={path} className="hover:underline hover:underline-offset-4 py-6">
+                  {title}
+                </Link>
+              ))}
+          {status === "authenticated" ? (
+            <LogOutButton />
+          ) : (
+            <Link href="/api/auth/signin" className="hover:underline hover:underline-offset-4 py-6">Увійти</Link>
+          )}
         </div>
       </nav>
     </>
