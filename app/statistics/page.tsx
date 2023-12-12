4ded11ae13d8d09c9d5fa153ad1843986a294ea5
Taras-Novitskyi <novitskyitaras@gmail.com>
2023-12-13 01:33:34 +0200

    added static page

--- app/statistics/page.tsx
+++ app/statistics/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState, useCallback, useEffect } from "react";
+import { useRouter } from "next/router";
 import axios from "axios";
 import { IHistoryResponce } from "@/globaltypes/historyTypes";
 import { useSession } from "next-auth/react";
@@ -8,21 +9,21 @@ axios.defaults.baseURL = "http://localhost:3000/";
 
 const SendingHistory = () => {
   const [history, setHistory] = useState<IHistoryResponce[]>([]);
-
+  const router = useRouter();
   const { data: session } = useSession();
 
   const xport = useCallback(async () => {
     try {
       const response = await axios.get(`api/sending-history`, {
         params: {
-          userId: 13,
+          userId: session?.user?.user_id,
         },
       });
       setHistory(response.data.history);
     } catch (error) {
       console.log(error);
     }
-  }, []);
+  }, [session?.user?.user_id]);
 
   useEffect(() => {
     xport();
@@ -44,7 +45,7 @@ const SendingHistory = () => {
     <div className="container xl mx-auto pb-20 pt-[60px]">
       <div className="flex items-center justify-end mb-[60px]">
         <div className="bg-[#B6B5B5] flex items-center justify-center h-[61px] w-[309px]  py-[14px] px-[60px] font-roboto text-[#372037] rounded-[18px] text-[22px]">
-          User name: 0 SMS
+          {session?.user?.user_name}: 0 SMS
         </div>
       </div>
       <h1 className="mb-3  font-roboto text-[40px]  text-[#372037] font-semibold">
@@ -52,35 +53,58 @@ const SendingHistory = () => {
       </h1>
       <button
         type="button"
+        onClick={() => console.log("archive")}
         className="bg-[#B6B5B5] flex items-center justify-center h-[35px] mb-[60px] py-2 px-2.5 font-roboto text-[#372037] rounded-[10px] text-[18px] border-[1px] border-black"
       >
         Дивитись архів
       </button>
 
       <div className="container flex flex-col pt-[60px] bg-[#D9D9D9] rounded-[10px]">
-        <p className="mb-10 px-6 font-roboto text-2xl font-medium text-[#372037]">
+        <p className="mb-10 mx-6 font-roboto text-2xl font-medium text-[#372037]">
           Пріод відправки SMS
         </p>
-        for{" "}
-        <p className="mb-8 text-emerald-400">
-          User: {session?.user?.user_login}
-        </p>
-        where{" "}
-        <p className="mb-8 text-emerald-400">
-          Id_User: {session?.user?.user_id}
-        </p>
+        <form className="container flex items-baseline gap-8 w-full mb-10 px-6 bg-[#D9D9D9] rounded-[10px]">
+          <input
+            type="date"
+            className="flex items-center justify-center h-[48px] w-[196px] py-2 px-3 font-montserrat bg-[#D9D9D9] text-[#372037] rounded-[18px] text-[18px] border-[1px] border-[#737373] cursor-pointer"
+          />
+          <div className="h-px w-6 bg-black"></div>
+          <input
+            type="date"
+            className="flex items-center justify-center h-[48px] w-[196px] py-2 px-3 font-montserrat bg-[#D9D9D9] text-[#372037] rounded-[18px] text-[18px] border-[1px] border-[#737373] cursor-pointer"
+          />
+          <button
+            type="submit"
+            className="flex items-center justify-center h-[63px] w-[208px] py-4 px-14 font-montserrat bg-[#D9D9D9] text-[#372037] rounded-[18px] text-[18px] border-[1px] border-[#636060]"
+          >
+            Дивитись
+          </button>
+        </form>
+
+        <div className="flex items-center gap-[100px] h-[57px] px-[26px] pt-[18px] pb-[13px] font-roboto text-[20px] text-black bg-[#BEBDBD] rounded-[18px]">
+          <p className="w-[194px]">Шлях відправлення</p>
+          <p className="w-[184px]">Дата</p>
+          <p className="w-[150px]">Відправленно </p>
+          <p className="w-[150px]">Отримано</p>
+        </div>
+
         <ul>
-          {history.map((item) => {
-            return (
-              <li key={item.history_id}>
-                <p>history_id = {item.history_id}</p>
-                <p>
-                  sending_group_date = {formatDateTime(item.sending_group_date)}
-                </p>
-                <p>group_name = {item.group_name}</p>
-              </li>
-            );
-          })}
+          {history &&
+            history.map((item) => {
+              return (
+                <li
+                  key={item.history_id}
+                  className="flex items-center gap-[100px] h-[57px] px-[26px] pt-[18px] pb-[13px] font-roboto text-[20px] text-black border-b border-black"
+                >
+                  <p className="w-[194px]">Невідомо</p>
+                  <p className="w-[184px]">
+                    {formatDateTime(item.sending_group_date)}
+                  </p>
+                  <p className="w-[150px]">{item.group_name}</p>
+                  <p className="w-[150px]">{item.group_name}</p>
+                </li>
+              );
+            })}
         </ul>
       </div>
     </div>
