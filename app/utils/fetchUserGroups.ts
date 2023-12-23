7b556f7271c5dd0342e66e438a8042810bd7f8f7
SerhiiSamara <ndaair@gmail.com>
2023-12-23 11:15:25 +0200

    Update Title in main page and update fetchUserGroups

--- app/utils/fetchUserGroups.ts
+++ app/utils/fetchUserGroups.ts
@@ -8,7 +8,7 @@ export default async function fetchUserGroups(id: number): Promise<QueryResult<I
 	const res: QueryResult<IGroupDatabase> = await db.query(
 		`SELECT send_groups.group_id, group_name, user_id, to_char(group_create_date, 'DD.MM.YYYY HH24:MI:SS') AS group_create_date, COUNT(groups_members.group_id)  AS number_members
 		FROM send_groups
-		INNER JOIN groups_members ON send_groups.group_id = groups_members.group_id
+		LEFT JOIN groups_members ON send_groups.group_id = groups_members.group_id
 		WHERE user_id = ${id}
 		GROUP BY send_groups.group_id`
 	);
