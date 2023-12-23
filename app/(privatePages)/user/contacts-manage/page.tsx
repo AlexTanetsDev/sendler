23de915691cd84a0924ab65c3df2e31f5442a8d7
SerhiiSamara <ndaair@gmail.com>
2023-12-23 15:03:49 +0200

    fix

--- app/(privatePages)/user/contacts-manage/page.tsx
+++ app/(privatePages)/user/contacts-manage/page.tsx
@@ -6,8 +6,9 @@ import { options } from "@/app/api/auth/[...nextauth]/options";
 import UserSmsBalansInform from '@/components/UserSmsBalansInform';
 import GroupsList from "@/components/groupsList";
 import CreateGroupForm from "@/components/forms/CreateGroupForm";
+import { getUserGroups } from "@/app/utils";
 
-import { ISession } from "@/globaltypes/types";
+import { ISession, IGroupDatabase } from "@/globaltypes/types";
 import Title from "@/components/Title";
 
 
@@ -15,6 +16,8 @@ export default async function ContactManagmentPage() {
 	const session: ISession | null = await getServerSession(options);
 
 	const userId = session?.user.user_id;
+
+	const userGroups: IGroupDatabase[] | undefined = await getUserGroups(userId);
 
 	if (!userId) {
 		console.log("Unable fetch userId!")
@@ -28,7 +31,7 @@ export default async function ContactManagmentPage() {
 			<div className='content-block mt-[53px]'>
 				<p className='w-1/2 mb-8 ml-8 text-lg font-normal font-roboto'>Для початку роботи Вам потрібно створити нову Групу контактів та додати до неї номери. Ви можете додати номери телефонів контактів з файлу у форматі Excel або текстового файлу.</p>
 				<CreateGroupForm id={userId} />
-				<GroupsList id={userId} />
+				<GroupsList groups={userGroups} />
 			</div>
 		</section>
 	)
