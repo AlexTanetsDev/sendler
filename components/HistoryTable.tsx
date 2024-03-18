5cfceb78586193fbf20f8d3e1667c33f10a8bbeb
Taras-Novitskyi <novitskyitaras@gmail.com>
2024-03-18 22:40:17 +0200

    updated download excel hisroty

--- components/HistoryTable.tsx
+++ components/HistoryTable.tsx
@@ -1,15 +1,14 @@
 'use client';
 
-import { useEffect, useState, useCallback } from 'react';
+import { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { getUserHistory } from '@/fetch-actions/historyFetchActions';
 import HistoryList from './HistoryList';
 import HistoryPeriodForm from './forms/HistoryPeriodForm';
-import { FormInputsPeriod } from './forms/HistoryPeriodForm';
 import { IHistoryResponce, IHistoryPeriod } from '@/globaltypes/historyTypes';
 
 type Props = {
-	id: number | undefined;
+  id: number | undefined;
 };
 
 export default function HistoryTable({ id }: Props) {
@@ -48,18 +47,18 @@ export default function HistoryTable({ id }: Props) {
     fetchAPI();
   }, [historyPeriod]);
 
-	return (
-		<>
-			<div className="content-block">
-				<HistoryPeriodForm />
-				<div className="flex items-center gap-[100px] h-[58px] px-[26px] font-roboto text-[20px] text-white bg-[#417D8A]">
-					<p className="w-[194px]">Шлях відправлення</p>
-					<p className="w-[184px]">Дата</p>
-					<p className="w-[150px]">Відправленно </p>
-					<p className="w-[150px]">Отримано</p>
-				</div>
-				<HistoryList userHistory={userHistory} />
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="content-block">
+        <HistoryPeriodForm />
+        <div className="flex items-center gap-[100px] h-[58px] px-[26px] font-roboto text-[20px] text-white bg-[#417D8A]">
+          <p className="w-[194px]">Шлях відправлення</p>
+          <p className="w-[184px]">Дата</p>
+          <p className="w-[150px]">Відправленно </p>
+          <p className="w-[150px]">Отримано</p>
+        </div>
+        <HistoryList userHistory={userHistory} />
+      </div>
+    </>
+  );
 }
